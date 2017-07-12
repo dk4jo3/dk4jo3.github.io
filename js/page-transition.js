3eeea86cff026fe8344c0e3b43f62e93c56a39be
dk4jo3 <dk4jo3@gmail.com>
2017-07-12 13:01:42 +0800

    create

---
+++ js/page-transition.js
@@ -0,0 +1,20 @@
+// page transition script 
+
+$(document).ready(
+    
+    function() {
+    $("body").css("display", "none");
+ 
+    $("body").fadeIn(2000);
+ 
+    $("a.transition").click(function(event){
+        event.preventDefault();
+        linkLocation = this.href;
+        $("body").fadeOut(2000, redirectPage);      
+    });
+         
+    function redirectPage() {
+        window.location = linkLocation;
+    }
+});
+ 
