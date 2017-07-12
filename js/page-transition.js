531fbacf73fe3e11211790a62038154ebb14e2b7
dk4jo3 <dk4jo3@gmail.com>
2017-07-12 13:49:05 +0800

    fade effect only .cover

--- js/page-transition.js
+++ js/page-transition.js
@@ -3,14 +3,14 @@
 $(document).ready(
     
     function() {
-    $("body").css("display", "none");
+    $(".cover").css("display", "none");
  
-    $("body").fadeIn(1000);
+    $(".cover").fadeIn(600);
  
     $("a.transition").click(function(event){
         event.preventDefault();
         linkLocation = this.href;
-        $("body").fadeOut(1000, redirectPage);      
+        $(".cover").fadeOut(600, redirectPage);      
     });
          
     function redirectPage() {
