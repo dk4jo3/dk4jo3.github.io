4d826867348e87ee0006eceb240be50969b6936a
dk4jo3 <dk4jo3@gmail.com>
2018-03-31 10:26:49 +0800

    minor fix
    singular, and reduce the scrolling speed.

--- js/works.js
+++ js/works.js
@@ -2,6 +2,14 @@
 
 //scrolling 
 
+      $(document).ready(function() {
+        $('#fullpage').fullpage({
+          scrollingSpeed: 400,
+          responsiveWidt: 768
+        });
+      });
+
+
 //lightbox 
 
 //page transition
