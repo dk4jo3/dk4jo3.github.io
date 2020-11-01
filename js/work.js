2938b7b28b09a77f2003df16d45265ebd38ea15b
dk4jo3 <dk4jo3@gmail.com>
2020-11-01 15:20:59 -0500

    20201101 Update
    Update Fullpage.js and Artfuse page. Added OTS page.

--- js/work.js
+++ js/work.js
@@ -10,8 +10,13 @@ $(document).on('click',
 // full page scrolling
 $(document).ready(function () {
     $('#fullpage').fullpage({
-        scrollingSpeed: 480,
-        responsiveWidth: 768
+        scrollingSpeed: 400,
+        responsiveWidth: 768,
+        easing: 'easeInOutCubic',
+        lazyLoading: true,
+        lockAnchors: false,
+        scrollBar: true,
+        anchors: ['main', 'page1', 'page2', 'page3', 'page4', 'page5', 'page6', 'page7', 'page8', 'page9', 'page10', 'page11', 'page12', 'page13', 'page14', 'page15', 'page16', 'page17', 'page18']
     });
 });
 
